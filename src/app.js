75f4a71adfc28d72a11422143ffcb7264e82da17
Wanderley Cabral da Silva <39313251+cabraldasilvac@users.noreply.github.com>
2024-04-25 13:54:09 -0300

    :rocket: => create a project

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const fastify = require('fastify')({ logger: false })
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const { basicAuth } = require('./middlewares/auth')
+
+// Import my routes
+const userRoutes = require('./routes/user.routes')
+const projectRoutes = require('./routes/project.routes')
+// Connect to my database
+mongoose
+    .connect(process.env.MONGODB_URI, {})
+    .then(() => console.log('Connected to the database'))
+    .catch((e) => console.log('Error connecting to database', e))
+
+// start my server
+fastify.register(userRoutes, { prefix: '/api/v1/users' })
+fastify.register(projectRoutes, { prefix: '/api/v1/projects' })
+
+// fastify.addHook('preHandler', basicAuth)
+
+const start = async () => {
+    try {
+        await fastify.listen(process.env.PORT || 5000)
+        fastify.log.info(`Server is running on port ${fastify.server.address().port}`)
+    } catch (error) {}
+}
+
+start()
---
+++ src/models/user.model.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        lowercase: true,
+    },
+    role: {
+        type: String,
+        enum: ['Admin', 'Project Manager', 'Team member'],
+        default: 'Team member',
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+    },
+})
+// 1 - Store the password safely in database
+UserSchema.pre('save', async function (next) {
+    try {
+        if (this.isModified('password') || this.isNew) {
+            const salt = await bcrypt.genSalt(10)
+            this.password = await bcrypt.hash(this.password, salt)
+        }
+    } catch (error) {
+        next(error)
+    }
+})
+
+UserSchema.methods.comparePassword = async function (password) {
+    try {
+        return await bcrypt.compare(password, this.password)
+    } catch (error) {
+        console.log(error)
+        throw error
+    }
+}
+
+const User = mongoose.model('User', UserSchema)
+
+module.exports = User
